e7eb865d959ce458fc42f11dcbb89b8f0e3f5f2d
AcharyaUjjal1 <acharya_ujjal@yahoo.com>
2018-04-25 13:21:10 +0545

    refactored code

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 import express from 'express';
-import React from 'react';
-import {renderToString} from 'react-dom/server';
-import Home from './client/components/Home';
+import renderer from './helpers/renderer'
 
 const app = express();
 const PORT = 3000;
@@ -9,19 +7,7 @@ const PORT = 3000;
 app.use(express.static('public'));
 
 app.get("/", (req, res)=>{
-    const content = renderToString(<Home />);
-
-    const html = `
-    <html>
-        <head></head>
-        <body>
-            <div id="root">${content}</div>
-            <script src="bundle.js"></script>
-        </body>
-    </html>
-    `
-
-    res.send(html);              
+    res.send(renderer());              
 })
 
 app.listen(PORT, ()=>{
