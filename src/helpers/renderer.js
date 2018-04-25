3276b1e026ce5e7816084990991d990b0b3e5572
AcharyaUjjal1 <acharya_ujjal@yahoo.com>
2018-04-25 15:31:55 +0545

    configured router

--- src/helpers/renderer.js
+++ src/helpers/renderer.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import {renderToString} from 'react-dom/server';
-import Home from '../client/components/Home';
+import {StaticRouter} from 'react-router-dom';
+import Routes from '../client/Routes'
 
-module.exports = ()=>{
-    const content = renderToString(<Home />);
+export default (req)=>{
+    const content = renderToString(
+        <StaticRouter location={req.path} context={{}}>
+            <Routes />
+        </StaticRouter>
+    );
     return `
     <html>
         <head></head>
--- src/index.js
+++ src/index.js
@@ -6,8 +6,8 @@ const PORT = 3000;
 
 app.use(express.static('public'));
 
-app.get("/", (req, res)=>{
-    res.send(renderer());              
+app.get("*", (req, res)=>{
+    res.send(renderer(req));              
 })
 
 app.listen(PORT, ()=>{
