1ba7d9e4c63e119fa356842c7763846d1e1e5874
AcharyaUjjal1 <acharya_ujjal@yahoo.com>
2019-04-18 16:17:58 +0545

    add reducers with actions

--- src/client/client.js
+++ src/client/client.js
@@ -8,6 +8,8 @@ import { createStore, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 import { Provider } from "react-redux";
 
+import reducers from './reducers';
+
 const store = createStore(reducers, {}, applyMiddleware(thunk));
 
 ReactDOM.hydrate(
